2217d9ad8a32574f3d383b861d804880444c7eac
vincentmachariakimani <vincentmachariakimani@gmail.com>
2025-06-11 16:59:12 +0300

    Migrations

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_restful import Api
+from flask_migrate import Migrate
+from Models.doctors import db
 from resourses.doctors import DoctorResource
 from resourses.patient import PatientResource
 from resourses.nurse import NurseResource
@@ -8,7 +10,18 @@ from resourses.billing import BillResource
 from resourses.diagnosis import DiagnosisResource
 
 app = Flask(__name__)
+
+# Cofiguring flask app using config object
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hospitalManagementSystem.db"
+
+# Linking flask resfull with app
 api = Api(app)
+
+# Creating a migrate object to mange migrations
+migrate = Migrate(app, db)
+
+# Link db with flask app
+db.init_app(app)
 
 
 api.add_resource(DoctorResource, "/doctors", "/doctors/<int:doctor_id>")
