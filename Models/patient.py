9ed2fcf2675d0e4d15d649cd02f882b92055b32a
ruthiguta <cirurue@gmail.com>
2025-06-12 07:21:52 +0300

    added the prescription and patient model

---
+++ Models/patient.py
@@ -0,0 +1,24 @@
+from sqlalchemy import MetaData, ForeignKey
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from doctors import Doctor
+
+metadata = MetaData()
+db = SQLAlchemy(metadata=metadata)
+
+
+class Patients(db.model):
+    __tablename__ = "patients"
+
+    id = db.Column(db.Integer, Primary_Key=True)
+    name = db.Column(db.Text, nullable=False)
+    diagnosis_id = db.Column(db.Integer)
+    dob = db.Column(db.Integer)
+    history = db.Column(db.String)
+    doctor_id = db.Column(db.integer, ForeignKey(Doctor.id))
+    created_at = db.Column(db.TIMESTAMP, default=datetime.now)
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    deleted_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    def __repr__(self):
+        return f"<Patient {self.name} {self.history}>"
